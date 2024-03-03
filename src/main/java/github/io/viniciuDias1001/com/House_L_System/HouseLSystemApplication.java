2d9121e562c83c7d6025a5ab115fbd648695fab5
Pedro Vinicius <pedrorochadias1001@gmail.com>
2024-03-03 18:55:22 -0300

    Start project

---
+++ src/main/java/github/io/viniciuDias1001/com/House_L_System/HouseLSystemApplication.java
@@ -0,0 +1,13 @@
+package github.io.viniciuDias1001.com.House_L_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseLSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseLSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/io/viniciuDias1001/com/House_L_System/HouseLSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.viniciuDias1001.com.House_L_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseLSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
