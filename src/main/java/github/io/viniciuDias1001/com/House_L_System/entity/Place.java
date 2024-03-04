a6210ba7d42a741e65af75e765a48585fa1a98a5
GLPontes <gabriel.pontes@dcx.ufpb.br>
2024-03-04 00:09:33 -0300

    Atualizado o Place

--- src/main/java/github/io/viniciuDias1001/com/House_L_System/entity/Place.java
+++ src/main/java/github/io/viniciuDias1001/com/House_L_System/entity/Place.java
@@ -11,5 +11,15 @@ public class Place {
 
     private String name;
 
-    public Place {}
+    public Place(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
