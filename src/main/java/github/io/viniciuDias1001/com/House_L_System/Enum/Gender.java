bdee7d73b131203a95023dd3e9fdc96ff3a49bd9
GLPontes <gabriel.pontes@dcx.ufpb.br>
2024-03-05 16:51:24 -0300

    Created the entity user and extras.

---
+++ src/main/java/github/io/viniciuDias1001/com/House_L_System/Enum/Gender.java
@@ -0,0 +1,16 @@
+package github.io.viniciuDias1001.com.House_L_System.Enum;
+
+public enum Gender {
+    Male("Male"),
+    DONE("Female");
+
+    private String gender;
+
+    Gender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
---
+++ src/main/java/github/io/viniciuDias1001/com/House_L_System/entity/User.java
@@ -0,0 +1,89 @@
+package github.io.viniciuDias1001.com.House_L_System.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import github.io.viniciuDias1001.com.House_L_System.Enum.Gender;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class User {
+
+
+    /*
+    USUÁRIO POSSUI
+    email
+    nome
+    Password
+    idade(data de nasc)
+    genero
+    */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Column(length = 25)
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Column(length = 50)
+    private String password;
+
+    @NotNull
+    @Column
+    @NotBlank
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private String birthdate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+}
