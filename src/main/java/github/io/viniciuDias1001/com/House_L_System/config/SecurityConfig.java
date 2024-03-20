9c192f526771ba6a198396b574345e262a7ab9ef
GLPontes <gabriel.pontes@dcx.ufpb.br>
2024-03-20 14:48:51 -0300

    Refactoring security

--- src/main/java/github/io/viniciuDias1001/com/House_L_System/config/SecurityConfig.java
+++ src/main/java/github/io/viniciuDias1001/com/House_L_System/config/SecurityConfig.java
@@ -1,9 +1,34 @@
 package github.io.viniciuDias1001.com.House_L_System.config;
 
 
+import org.springframework.context.annotation.Bean;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder passwordEncoder = new PasswordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+    }
 
 }
