6f8d2d48e3d3b67607c7744c6ca9dd2e71733b51
GLPontes <gabriel.pontes@dcx.ufpb.br>
2024-03-20 13:56:31 -0300

    Refactoring security

---
+++ src/main/java/github/io/viniciuDias1001/com/House_L_System/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package github.io.viniciuDias1001.com.House_L_System.config;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
